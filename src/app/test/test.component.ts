1469a92042817b5fc9638aab423f45e362e59f8d
Shishir Bagalkot <59408046+Shishirbagalkot@users.noreply.github.com>
2022-04-12 09:39:39 +0530

    Style Binding and Event Binding

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -7,11 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestComponent implements OnInit {
 
-  
+  //interpolation
+  public interpolationExample = "Test for interpolation"
+
+  //property binding
+  public propertyBindingExample = "Test for property binding"
+
+  //class binding
+  public classBindingExample = "text-success"
+  public hasError = true
+  public isSpecial = true
+  public messageClasses = {
+    "text-success": !this.hasError,
+    "text-danger": this.hasError,
+    "text-special": this.isSpecial
+  }
+
+  //style binding
+  public titleStyles = {
+    color: "blue",
+    fontStyle: "italic"
+  }
+
+  //event binding
+  public greeting = ""
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onClick() {
+    this.greeting = "Welcome to Angular"
+  }
 }
